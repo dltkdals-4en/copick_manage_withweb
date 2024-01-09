0eee94945792f4000bb08fd32601467f85095eaf
DESKTOP-S4PLJ2V\LeeSangmin <dltkdalsdla@4en.co.kr>
2024-01-09 15:45:23 +0900

    [기능 추가] - 메인 UI 변경 - 태스크 일괄 추가 기능 추가 -> 해당 요일에 중복된 태스크 추가 불가하게 변경 - 기존 페이지에서 요일별 페이지로 변경 - 태스크 삭제시 첫화면으로 돌아가지 않게 변경

--- lib/pages/task_add_page/task_add_page.dart
+++ lib/pages/task_add_page/task_add_page.dart
@@ -1,3 +1,6 @@
+import 'package:copick_manage_withweb/pages/task_add_page/widgets/add_item_widget.dart';
+import 'package:copick_manage_withweb/pages/task_add_page/widgets/task_add_cafelist_widget.dart';
+import 'package:copick_manage_withweb/pages/task_add_page/widgets/task_add_reservelist_widget.dart';
 import 'package:copick_manage_withweb/provider/task_manage_provider.dart';
 import 'package:copick_manage_withweb/utilitys/colors.dart';
 import 'package:copick_manage_withweb/utilitys/constants.dart';
@@ -9,179 +12,22 @@ class TaskAddPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var taskProvider = Provider.of<TaskManageProvider>(context);
-    var items = taskProvider.addList;
-    var size = MediaQuery.of(context).size;
-    var tItems = taskProvider.addTList;
-    var fItems = taskProvider.addFList;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('태스크 추가하기'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(NORMALGAP),
+      body: const Padding(
+        padding: EdgeInsets.all(NORMALGAP),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text('요일 선택'),
-                    DropdownButton(
-                      value: taskProvider.selectedWeek,
-                      items: taskProvider.weekDay.map((e) {
-                        return DropdownMenuItem<String>(
-                            value: e, child: Text(e));
-                      }).toList(),
-                      onChanged: (value) {
-                        taskProvider.changeWeek(value);
-                      },
-                    ),
-                  ],
-                ),
-                kBigW,
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text('팀 선택'),
-                    DropdownButton(
-                      value: taskProvider.selectedTeam,
-                      items: taskProvider.team.map((e) {
-                        return DropdownMenuItem<String>(
-                            value: e, child: Text(e));
-                      }).toList(),
-                      onChanged: (value) {
-                        taskProvider.changeTeam(value);
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            ),
             Expanded(
               child: Row(
                 children: [
-                  Container(
-                    width: (size.width - 70) / 2,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: KColors.lightPrimary,
-                        width: 1,
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(NORMALGAP),
-                      child: ListView.separated(
-                        // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        //     crossAxisCount: 3,
-                        //     childAspectRatio: 3,
-                        //     crossAxisSpacing: NORMALGAP,
-                        //     mainAxisSpacing: NORMALGAP),
-                        separatorBuilder: (context, index) => Divider(),
-                        itemCount: fItems.length,
-                        padding: EdgeInsets.only(bottom: SMALLGAP),
-                        itemBuilder: (context, index) {
-                          var item = fItems[index];
-                          return ListTile(
-                            title: Text(
-                                '${item['waste'].locationName} // ${item['state']}'),
-                            onTap: () {
-                              taskProvider.changeState(item);
-                            },
-                            contentPadding: EdgeInsets.all(NORMALGAP),
-                          );
-                          // return CheckboxListTile(
-                          //   value: item['state'],
-                          //   onChanged: (value) {
-                          //     taskProvider.changeState(item, value);
-                          //   },
-                          //   title: Text(
-                          //       '${item['waste'].locationName} // ${item['state']}'),
-                          //   controlAffinity: ListTileControlAffinity.platform,
-                          //   // shape: RoundedRectangleBorder(
-                          //   //   side: BorderSide(color: KColors.black, width: 1,),
-                          //   //   borderRadius: BorderRadius.circular(SMALLGAP),
-                          //   // ),
-                          //
-                          //   contentPadding: EdgeInsets.all(NORMALGAP),
-                          // );
-                        },
-                      ),
-                    ),
-                  ),
-                  // kSmW,
-                  // Container(
-                  //   child: Column(
-                  //     mainAxisSize: MainAxisSize.min,
-                  //     children: [
-                  //       ElevatedButton(
-                  //         onPressed: () {},
-                  //         child: Text('추가'),
-                  //       ),
-                  //       ElevatedButton(
-                  //         onPressed: () {},
-                  //         child: Text('제거'),
-                  //       ),
-                  //     ],
-                  //   ),
-                  // ),
-                  // kSmW,
+                  TaskAddCafelistWidget(),
                   kBigW,
-                  Container(
-                    width: (size.width - 70) / 2,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: KColors.lightPrimary,
-                        width: 1,
-                      ),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(NORMALGAP),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          Container(
-                            child: Text(
-                              '추가 탭 \n ${tItems.length}개 추가됨',
-                              style: kHeaderTextStyle.copyWith(),
-                            ),
-                          ),
-                          Expanded(
-                            child: ListView.separated(
-                              itemCount: tItems.length,
-                              separatorBuilder: (context, index) => Divider(),
-                              padding: EdgeInsets.only(bottom: SMALLGAP),
-                              itemBuilder: (context, index) {
-                                var item = tItems[index];
-                                return ListTile(
-                                  title: Text(
-                                      '${item['waste'].locationName} // ${item['state']}'),
-                                  onTap: () {
-                                    taskProvider.changeState(item);
-                                  },
-                                  contentPadding: EdgeInsets.all(NORMALGAP),
-                                );
-                              },
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {},
-                                child: Text('경로 추가하기'),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  )
+                  TaskAddReservelistWidget(),
                 ],
               ),
             ),
---
+++ lib/pages/task_add_page/widgets/task_add_cafelist_widget.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../provider/task_manage_provider.dart';
+import '../../../utilitys/colors.dart';
+import '../../../utilitys/constants.dart';
+import 'add_item_widget.dart';
+import 'check_dialog_widget.dart';
+
+class TaskAddCafelistWidget extends StatelessWidget {
+  const TaskAddCafelistWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var taskProvider = Provider.of<TaskManageProvider>(context);
+    var size = MediaQuery
+        .of(context)
+        .size;
+    var tItems = taskProvider.addTList;
+    var fItems = taskProvider.addFList;
+    return Container(
+      width: (size.width - 70) / 2,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: KColors.lightPrimary,
+          width: 1,
+        ),
+        color: KColors.whiteGrey,
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: (size.width - 70) / 2,
+            height: 70,
+            color: KColors.lightPrimary,
+            child: Center(
+              child: Text(
+                '추가하실 카페를 클릭해주세요.',
+                style: kLabelTextStyle.copyWith(
+                  color: KColors.white,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(NORMALGAP),
+              child: Column(
+                children: [
+                  Container(
+                    color: KColors.white,
+                    child: TextField(
+                      maxLines: 1,
+                      onChanged: (value) {
+                        taskProvider.searchCafe(value);
+                      },
+                    ),
+                  ),
+                  kNorH,
+                  Expanded(
+                    child: ListView.separated(
+                      separatorBuilder: (context, index) => Divider(),
+                      itemCount: fItems.length,
+                      padding: EdgeInsets.only(bottom: SMALLGAP),
+                      itemBuilder: (context, index) {
+                        var item = fItems[index];
+                        return AddItemWidget(
+                          item: item,
+                          onTap: () async {
+
+                                switch (taskProvider.addValidation(item['waste'].locationId)) {
+                                  case 0:
+                                    showDialog(context: context, builder: (context) => CheckDialogWidget(title: '추가 불가',contents: '수거요일과 팀을 선택해주세요.',),);
+                                    break;
+                                  case 1:
+                                    showDialog(context: context, builder: (context) => CheckDialogWidget(title: '추가 불가',contents: '이미 등록되어 있어요.',),);
+                                    break;
+                                  case 2:
+                                    taskProvider.changeState(item);
+                                    break;
+                                }
+                                // if (taskProvider.addValidation()) {
+                                //   taskProvider.changeState(item);
+                                // } else {
+                                //   showDialog(context: context, builder: (context) => CheckDialogWidget(),);
+                                //
+                                //   // await FlutterPlatformAlert.showAlert(windowTitle: '태스크 추가 안됨', text: '수거요일과 팀을 선택해주세요.');
+                                // }
+
+                                // taskProvider.addToReserve(item);
+                            },
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
